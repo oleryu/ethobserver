7345d961f2e98cc1c6828f6e3f17062239597e7e
oleryu@aliyun.com <oleryu@aliyun.com>
2018-10-23 14:08:20 +0800

    2018-10-10

--- src/main/java/xyz/oleryu/wallet/eth/observer/EthobserverApplication.java
+++ src/main/java/xyz/oleryu/wallet/eth/observer/EthobserverApplication.java
@@ -1,7 +1,9 @@
 package xyz.oleryu.wallet.eth.observer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @ oleryu.xyz
